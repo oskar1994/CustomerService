78278ce90c1f07a2f9ccb1d1c3b965cdef30c924
Oskar Strzelecki <oskar.strz@gmail.com>
2018-09-12 22:40:50 +0200

    CustomerService project fires MVVM at startup, added observable object which implements INotifyPropertyChanged interface and tested its implementation

---
+++ MVVM/ObservableObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+            #region Events
+            public event PropertyChangedEventHandler PropertyChanged;
+            #endregion
+
+            #region Methods
+            public bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+            {
+                if (Equals(storage, value))
+                {
+                    return false;
+                }
+                storage = value;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
---
+++ MVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            Test = "aaaaaaa";
+        }
+        #endregion
+
+        #region Fields
+        private string _test;
+        #endregion
+
+        #region Properties
+        public string Test
+        {
+            get
+            {
+                return _test;
+            }
+            set
+            {
+                SetProperty(ref _test, value);
+            }
+        }
+
+        #endregion
+    }
+}
