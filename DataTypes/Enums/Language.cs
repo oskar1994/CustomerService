da060c7f844e4efd0a34a27fac5e359f1626cc40
Oskar Strzelecki <oskar.strz@gmail.com>
2018-09-12 22:10:16 +0200

    Lang engine finished

---
+++ DataTypes/Enums/Language.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes.Enums
+{
+    public enum Language
+    {
+        Custom = ~0,
+        Polish
+    }
+}
---
+++ LangDictionary/LangConfigurator.cs
@@ -0,0 +1,37 @@
+﻿using DataTypes.Enums;
+using LangDictionary.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LangDictionary
+{
+    /// <summary>    
+    /// responsible for manipulating languages ​​in the program
+    /// </summary>
+    public class LangConfigurator
+    {
+        #region Methods
+        public static void SetLang(Language type)
+        {
+            switch (type)
+            {
+                case Language.Custom:
+                    ConfigureResourcesOnSelectedLang("");
+                    break;
+                case Language.Polish:
+                    ConfigureResourcesOnSelectedLang("pl-PL");
+                    break;             
+            }
+        }
+
+        private static void ConfigureResourcesOnSelectedLang(string lang)
+        {
+            
+            Resource.Culture = new System.Globalization.CultureInfo(lang);
+        }
+        #endregion
+    }
+}
