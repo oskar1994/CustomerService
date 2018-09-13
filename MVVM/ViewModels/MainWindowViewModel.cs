aa7896bbd146b1f3a6b4b086d11ad295aca83301
Oskar Strzelecki <oskar.strz@gmail.com>
2018-09-13 23:17:15 +0200

    Created XAML for dynamic generated buttons

--- MVVM/ViewModels/MainWindowViewModel.cs
+++ MVVM/ViewModels/MainWindowViewModel.cs
@@ -14,40 +14,16 @@ namespace MVVM.ViewModels
         #region Constructor
         public MainWindowViewModel()
         {
-            Test = "aaaaaaa";
-            try
-            {
-                using (DataContext db = new DataContext())
-                {
-                    db.CustomerController.AddCustomer(new Customer() { Address = "sad", LastName = "sd", Name = "sdsad", TelephoneNumber = 222});
-                    Customer a = null;
-                    a.Name = "asd";
-                }
-            }
-            catch(Exception exc)
-            {
-                Logger.LogException(exc);
-            }
+          
         }
         #endregion
 
         #region Fields
-        private string _test;
+      
         #endregion
 
         #region Properties
-        public string Test
-        {
-            get
-            {
-                return _test;
-            }
-            set
-            {
-                SetProperty(ref _test, value);
-            }
-        }
-
+       
         #endregion
     }
 }
