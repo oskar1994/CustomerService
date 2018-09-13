4b8276e444e13b8b14fe04c65136210b4e2be36f
Oskar Strzelecki <oskar.strz@gmail.com>
2018-09-13 20:18:41 +0200

    EF DbContext  and SQLite

---
+++ DatabaseEngine/Contexts/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseEngine.Contexts
+{
+    /// <summary>
+    /// EF DbContext and SQLite
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        public DataContext() : base(new SQLiteConnection()
+        {
+            ConnectionString = new SQLiteConnectionStringBuilder()
+            {
+                DataSource = System.IO.Path.GetTempPath() + "\\CustomerService.csdb" 
+            }.ConnectionString
+        } , true)
+        {
+
+        }
+    }
+}
