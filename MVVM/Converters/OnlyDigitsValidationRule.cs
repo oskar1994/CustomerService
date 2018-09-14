da5067d07f36523dd0d371ca8101108174b76d2f
Oskar Strzelecki <oskar.strz@gmail.com>
2018-09-14 18:05:48 +0200

    validation at phone number

---
+++ MVVM/Converters/OnlyDigitsValidationRule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MVVM.Converters
+{
+    public class OnlyDigitsValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var validationResult = new ValidationResult(true, null);
+
+            if (value != null)
+            {
+                if (!string.IsNullOrEmpty(value.ToString()))
+                {
+                    var regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+                    var parsingOk = !regex.IsMatch(value.ToString());
+                    if (!parsingOk)
+                    {
+                        validationResult = new ValidationResult(false, String.Empty);
+                    }
+                }
+            }
+
+            return validationResult;
+        }
+    }
+}
