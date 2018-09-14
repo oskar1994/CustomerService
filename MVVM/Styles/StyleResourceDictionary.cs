1c08d03d9869f4334f570f6d7ecc4fac078c4fd8
Oskar Strzelecki <oskar.strz@gmail.com>
2018-09-14 11:07:49 +0200

    introduction to styles and vector icons

---
+++ MVVM/Styles/StyleResourceDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM.Styles
+{
+    public static class StyleResourceDictionary
+    {
+        public static void InitStyleResourcesDictionary()
+        {
+            _styles = new ResourceDictionary();
+            _styles.Source = new Uri("/MVVM;component/Styles/Styles.xaml", UriKind.RelativeOrAbsolute);
+        }
+        public static ResourceDictionary _styles;
+    }
+}
--- MVVM/ViewModels/MainWindowViewModel.cs
+++ MVVM/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using Domain;
 using Helpers;
 using MVVM.CustomControls;
+using MVVM.Styles;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,6 +17,7 @@ namespace MVVM.ViewModels
         #region Constructor
         public MainWindowViewModel()
         {
+            StyleResourceDictionary.InitStyleResourcesDictionary();
             InitButtons();          
         }
         #endregion
@@ -32,7 +34,34 @@ namespace MVVM.ViewModels
         private void InitButtons()
         {
             CustomButtonPanel = new CustomButtonPanel();
-            CustomButtonPanel.CustomButtons.Add(new CustomButton { });
+            var addButton = new CustomButton()
+            {
+                ToolTip = "Add",
+                Icon = StyleResourceDictionary._styles["Add"] as System.Windows.Media.Geometry,
+                IsEnabled = true,
+                Opacity = 100,
+                Command = new RelayCommand<object>(x => System.Windows.MessageBox.Show("Add"))
+            };
+            var editButton = new CustomButton()
+            {
+                ToolTip = "Edit",
+                Icon = StyleResourceDictionary._styles["Edit"] as System.Windows.Media.Geometry,
+                IsEnabled = true,
+                Opacity = 100,
+                Command = new RelayCommand<object>(x => System.Windows.MessageBox.Show("Edit"))
+            };
+            var deleteButton = new CustomButton()
+            {
+                ToolTip = "Delete",
+                Icon = StyleResourceDictionary._styles["Delete"] as System.Windows.Media.Geometry,
+                IsEnabled = true,
+                Opacity = 100,
+                Command = new RelayCommand<object>(x => System.Windows.MessageBox.Show("Delete"))
+            };
+
+            CustomButtonPanel.CustomButtons.Add(addButton);
+            CustomButtonPanel.CustomButtons.Add(editButton);
+            CustomButtonPanel.CustomButtons.Add(deleteButton);
         }
         #endregion
     }
